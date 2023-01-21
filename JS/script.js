39a1ca1f0336b9b5a237cffc779849aeeee9879d
Felipe Oliveira <lipe111094@hotmail.com>
2023-01-21 19:02:03 -0300

    Create Project

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+const form= document.querySelector('#form-habits');
+const nlwSetup = new NLWSetup(form);
+
+const data ={
+    run: ['01-01', '01-02', '01-06'],
+    water:['01-04', '01-05'],
+    food:['01-01', '01-03']
+}
