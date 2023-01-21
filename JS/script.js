f9fc8804acbf139221bae49dcf8aaee0f727b709
Felipe <lipe111094@hotmail.com>
2023-01-21 19:49:50 -0300

    Configuration Display Screen Seup

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,20 @@
-const form= document.querySelector('#form-habits');
+const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data ={
-    run: ['01-01', '01-02', '01-06'],
-    water:['01-04', '01-05'],
-    food:['01-01', '01-03']
+button.addEventListener("click", add);
+
+function add(){
+    alert('Você Clicou no Botão');
 }
+
+/*const data = {
+  run: ["01-01", "01-05"],
+  water: ["01-02", "01-04"],
+  food: ["01-03"],
+  journal: ["01-02", "01-04"],
+  takePills: ["01-01", "01-05"],
+};
+
+nlwSetup.setData(data);
+nlwSetup.load();*/
