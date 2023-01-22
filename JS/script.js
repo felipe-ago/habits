99d2de5e3122ccd60ef013f653a27fba7277bc1f
Felipe <lipe111094@hotmail.com>
2023-01-22 01:44:09 -0300

    Configuring JavaScript for add dyas and saving habits

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,30 @@
+//Script para Funcionalidade do Projeto
+//Constantes para alteragção da Tela
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
 const button = document.querySelector("header button");
 
+//Funcionalidades ao clicar no botão e Salvar a Tela ao ser alterada
 button.addEventListener("click", add);
+form.addEventListener("change", save);
 
-function add(){
-    alert('Você Clicou no Botão');
+//Função de Adcionar dia
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Dia já Incluso! Favor Altere o Dia. 📅");
+    return;
+  }
+
+  alert("Dia Adicionado com Sucesso! ✔");
+  nlwSetup.addDay(today);
+}
+
+//Função para salvar Tela ao ser alterada utilizando o Local Storage do Browser
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
 }
 
 /*const data = {
@@ -14,7 +33,9 @@ function add(){
   food: ["01-03"],
   journal: ["01-02", "01-04"],
   takePills: ["01-01", "01-05"],
-};
+};*/
 
+//Comandos para carregar tela com as alterações salvas
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
 nlwSetup.setData(data);
-nlwSetup.load();*/
+nlwSetup.load();
